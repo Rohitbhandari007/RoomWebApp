6104d2b8246c2ddaa451585d73d25621eb76a0fb
Rohitbhandari007 <46572686+Rohitbhandari007@users.noreply.github.com>
2020-10-16 22:05:48 +0545

    init

--- backend/src/kothakhali/asgi.py
+++ backend/src/kothakhali/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for roomfinder project.
+ASGI config for kothakhali project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roomfinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kothakhali.settings')
 
 application = get_asgi_application()
