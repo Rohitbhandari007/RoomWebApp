1ad78274fe1085b851fde9348a76b54c0e5a0245
Rohitbhandari007 <46572686+Rohitbhandari007@users.noreply.github.com>
2020-10-16 16:19:49 +0545

    init

--- backend/src/core/views.py
+++ backend/src/core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-def test_view(request):
-    data = {
-        'name':'rohit',
-        'age':'20'
-    }
-    return JsonResponse(data)
+#third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data={
+            'name':'rohit',
+            'age': '20'
+        }
+        return Response(data)
