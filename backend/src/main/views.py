a4edc7466490300c914fc472992d4ef9e0f405d3
Rohitbhandari007 <46572686+Rohitbhandari007@users.noreply.github.com>
2020-10-17 16:10:32 +0545

    initializing new app

--- backend/src/main/views.py
+++ backend/src/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def test_view(request):
+    data = {
+        'name':'rohit',
+        'age':23
+    }
+    return JsonResponse(data)
