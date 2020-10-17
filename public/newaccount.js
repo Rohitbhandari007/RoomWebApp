f696937f1538ad8640aabea42cf804053b36bbd2
Suzzit Bajagain <bajagainsuzzit@gmail.com>
2020-10-17 14:04:34 +0545

    bug fixed

--- public/newaccount.js
+++ public/newaccount.js
@@ -4,26 +4,31 @@ const loginButton = document.querySelector("#loginbtn");
 const usernameField = document.querySelector("#username");
 
 
-loginButton.addEventListener("click", (e)=>{
+loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     let email = emailField.value;
     let password = passwordField.value;
     let username = usernameField.value;
-    let data = {email, username, password};
+    let data = { email, username, password };
     let contstraints = {
         method: "POST",
-        headers:{
-            "content-type": "application/json" 
+        headers: {
+            "content-type": "application/json"
         },
         body: JSON.stringify(data)
     }
-    fetch("/api", contstraints)
-    .then((res)=>{
-        res.json();
-    }).then((data)=>{
-        console.log("Data from Server = "+ data)
-    }).catch((error)=>{
-        console.log("An error occured" + error);
-    });
-    };
+
+    fetch("http://127.0.0.1:8000/api/register/", contstraints)
+        .then((res) => {
+            res.json();
+        })
+        .then((data) => {
+            console.log("Data from Server = " + data);
+        })
+        .catch((error) => {
+            console.log("An error occured" + error);
+        });
+    
 })
+    
+    
